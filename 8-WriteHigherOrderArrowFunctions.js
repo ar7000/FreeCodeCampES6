260844ae89f3669a531de76af22f695379ac0ef7
Ash Robinson <a_p_robinson@icloud.com>
2019-10-05 07:31:13 +0100

    Completed initial tasks on variables and functions

---
+++ 8-WriteHigherOrderArrowFunctions.js
@@ -0,0 +1,11 @@
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = arr.filter(a => a > 0 && Number.isInteger(a)).map(a => a * a);
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
---
+++ README.md
@@ -0,0 +1,1 @@
+Revisiting ES6 via FreeCodeCamp resources and copying passed code to Git repo as I go through each task. Documenting below anything new I learn, or anything I am reminded of that I may have forgotten.

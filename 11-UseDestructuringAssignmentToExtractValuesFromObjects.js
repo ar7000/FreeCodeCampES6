f763ecdd5093cc8ea5b1290c82f5dc1f06c48f9c
Ash Robinson <a_p_robinson@icloud.com>
2019-10-16 19:50:17 +0100

    Removed comments from task 11.

--- 11-UseDestructuringAssignmentToExtractValuesFromObjects.js
+++ 11-UseDestructuringAssignmentToExtractValuesFromObjects.js
@@ -1,15 +1,13 @@
 const HIGH_TEMPERATURES = {
-  yesterday: 75,
-  today: 77,
-  tomorrow: 80
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
 };
 
-// change code below this line
 
-const {today, tomorrow} = HIGH_TEMPERATURES;
+const { today, tomorrow } = HIGH_TEMPERATURES;
 
-// change code above this line
 
-console.log(yesterday) // should be not defined
-console.log(today); // should be 77
-console.log(tomorrow); // should be 80
+console.log(yesterday);
+console.log(today);
+console.log(tomorrow);
